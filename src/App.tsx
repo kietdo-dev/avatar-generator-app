29133b46fe6522a98c549efe3de3240727509ec0
teq-kietdo <kietdo@teqnological.asia>
2025-08-11 09:57:24 +0700

    Initial commit: Avatar Generator web app with customizable facial features

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import AvatarGenerator from './components/AvatarGenerator'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <AvatarGenerator />
+    </div>
+  )
+}
+
+export default App
